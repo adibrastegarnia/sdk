62b31bbb2057fb5d3060b45c96045456e9fe2cf5
Jordan Halterman <jordan.halterman@intel.com>
2022-05-03 13:32:57 -0700

    Consolidate runtime server into service

--- pkg/runtime/service.go
+++ pkg/runtime/service.go
@@ -4,35 +4,50 @@
 
 package runtime
 
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+)
+
 func NewService(runtime *Runtime, opts ...Option) *Service {
 	var options Options
 	options.apply(opts...)
 	return &Service{
 		Options: options,
+		runtime: runtime,
+		server:  grpc.NewServer(),
 	}
 }
 
 type Service struct {
 	Options
 	runtime *Runtime
-	server  *Server
+	server  *grpc.Server
 }
 
 func (s *Service) Start() error {
-	s.runtime = New(WithOptions(s.Options))
 	if err := s.runtime.Run(); err != nil {
 		return err
 	}
-	s.server = newServer(s.Server)
 	for _, f := range s.Atoms {
-		f(s.server.Server, s.runtime)
+		f(s.server, s.runtime)
 	}
-	if err := s.server.start(); err != nil {
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Server.Host, s.Server.Port))
+	if err != nil {
 		return err
 	}
+
+	go func() {
+		if err := s.server.Serve(lis); err != nil {
+			panic(err)
+		}
+	}()
 	return nil
 }
 
 func (s *Service) Stop() error {
+	s.server.Stop()
 	return s.runtime.Shutdown()
 }
